c8a79ea2952d82eabaf252c954dcc640b299cf15
Iskers <oskar.t.inderberg@hotmail.com>
2020-03-06 21:49:32 +0100

    Starting on final changes to main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import module.file_handler as fh
+import module.path_finder as pf
 import module.parsers as pars
+import module.circuit as cir
 
 if __name__ == '__main__':
+    # Define circuit origin file.
+    file_name = "task_circuit.tsv"
+
+    # Create utility classes
+    parser = pars.Parser()
+
+    # Create circuit class instance
+    # The parse function takes in file format and
+    circuit = parser.parse(file_name, "tsv", False)
+
+
+    cir.Circuit
+
     file_name = "task_circuit.tsv"
     parser = pars.Parser()
     circuit = parser.parse(file_name, "tsv", False)
